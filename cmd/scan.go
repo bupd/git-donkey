a51376c99b183c480fc3505386d157e79ce9093d
bupd <bupdprasanth@gmail.com>
2024-05-11 04:09:22 +0530

    update scan

--- cmd/scan.go
+++ cmd/scan.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/bupd/git-donkey/cmd/ui/listdirs"
+	"github.com/bupd/git-donkey/cmd/ui/multiinputs"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
@@ -20,6 +23,11 @@ func init() {
 }
 
 func scan(cmd *cobra.Command, args []string) {
+	p := tea.NewProgram(multiinputs.InitialModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+
 	listdirs.RunModel()
-	fmt.Println("kumaaruuu")
 }
