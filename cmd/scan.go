aa018194539df5ea37e9734fef13ae3977bcdf43
bupd <bupdprasanth@gmail.com>
2024-05-11 09:23:36 +0530

    update scan function

--- cmd/scan.go
+++ cmd/scan.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/bupd/git-donkey/cmd/ui/listdirs"
+	"github.com/bupd/git-donkey/cmd/program"
 	"github.com/bupd/git-donkey/cmd/ui/multiinputs"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
@@ -23,11 +23,20 @@ func init() {
 }
 
 func scan(cmd *cobra.Command, args []string) {
-	p := tea.NewProgram(multiinputs.InitialModel(), tea.WithAltScreen())
+	// Initialize GitInfo struct
+	gitInfo := program.GitInfo{}
+	// get the git dirs
+	gitInfo.TotalGits, gitInfo.GitDirs = program.GitDirs()
+	gitInfo.Untracked = program.UntrackedChanges(gitInfo.GitDirs)
+	gitInfo.TotalUntracked = len(gitInfo.Untracked)
+	gitInfo.Uncommitted = program.UncommittedChanges(gitInfo.GitDirs)
+	gitInfo.TotalUncommitted = len(gitInfo.Uncommitted)
+	gitInfo.Unpushed = program.UnpushedChanges(gitInfo.GitDirs)
+	gitInfo.TotalUnpushed = len(gitInfo.Unpushed)
+
+	p := tea.NewProgram(multiinputs.InitialModel(gitInfo), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
-
-	listdirs.RunModel()
 }
