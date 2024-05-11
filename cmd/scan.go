18541d60005956656c841692ab8dc1b01889b31f
bupd <bupdprasanth@gmail.com>
2024-05-12 04:32:46 +0530

    add: logo to the output

--- cmd/scan.go
+++ cmd/scan.go
@@ -7,6 +7,7 @@ import (
 	"github.com/bupd/git-donkey/cmd/program"
 	"github.com/bupd/git-donkey/cmd/ui/multiinputs"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 )
 
@@ -17,6 +18,35 @@ var scanCmd = &cobra.Command{
 
 	Run: scan,
 }
+
+var logo = `
+
+
+                          /\          /\
+                         ( \\        // )
+                          \ \\      // /
+                           \_\\||||//_/
+                            \/ _  _ \
+                           \/|(O)(O)|
+          ❤️ bupd         \/ |      |
+      ___________________\/  \      /
+     //                //     |____|
+    //                ||     /      \
+   //|                \|     \ 0  0 /
+  // \       )         V    / \____/
+ //   \     /        (     /
+""     \   /_________|  |_/
+       /  /\   /     |  ||
+      /  / /  /      \  ||
+      | |  | |        | ||
+      | |  | |        | ||
+      |_|  |_|        |_||
+       \_\  \_\        \_\\
+
+  
+  `
+
+var logoStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("81")).Bold(true)
 
 func init() {
 	rootCmd.AddCommand(scanCmd)
@@ -33,6 +63,8 @@ func scan(cmd *cobra.Command, args []string) {
 	gitInfo.TotalUncommitted = len(gitInfo.Uncommitted)
 	gitInfo.Unpushed = program.UnpushedChanges(gitInfo.GitDirs)
 	gitInfo.TotalUnpushed = len(gitInfo.Unpushed)
+
+	fmt.Printf("%s\n", logoStyle.Render(logo))
 
 	p := tea.NewProgram(multiinputs.InitialModel(gitInfo))
 	if _, err := p.Run(); err != nil {
